cd0f9524f99a5373bf2a8700dee5ca93af2e52f2
WaweruAntony <j.antony712@gmail.com>
2023-05-06 19:13:48 +0300

    added content on the page

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-// close button clicked
+// close icon clicked
 const closeBtn = document.querySelector("#close");
 closeBtn.addEventListener("click", () => {
-  console.log(closeBtn);
-  const menu = document.querySelector(".mobile-menu");
-  const menuIcon = document.querySelector(".menu-icon");
-  menu.classList.remove("display-mobile-menu");
-  menuIcon.classList.remove("hide-icon");
+  handleClicks(closeBtn);
 });
-// menu button clicked
+// menu icon clicked
 const menuBtn = document.querySelector("#menu");
 menuBtn.onclick = () => {
+  handleClicks(menuBtn);
+};
+
+//handles all the links of navigation
+const handleClicks = (button) => {
   const menu = document.querySelector(".mobile-menu");
   const menuIcon = document.querySelector(".menu-icon");
-  menu.classList.add("display-mobile-menu");
-  menuIcon.classList.add("hide-icon");
+
+  //menu icon is clicked
+  if (button.id === "menu") {
+    menu.classList.add("display-mobile-menu");
+    menuIcon.classList.add("hide-icon");
+
+    //any item clicked
+    const navs = document.querySelectorAll("#nav");
+    console.log(navs);
+    navs.forEach((item) => {
+      item.onclick = () => {
+        menu.classList.remove("display-mobile-menu");
+        menuIcon.classList.remove("hide-icon");
+      };
+    });
+  }
+
+  //close icon is clicked
+  if (button.id === "close") {
+    menu.classList.remove("display-mobile-menu");
+    menuIcon.classList.remove("hide-icon");
+  }
 };
+
+const date = new Date().getFullYear();
+
+console.log(date);
+document.getElementById("em").innerHTML = date;
